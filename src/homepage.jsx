2a4016512529adb8501021e3049b0eb20b3cc370
vladantosev <vladantosev@yahoo.com>
2019-12-30 21:39:25 +0100

    Added homepage component

---
+++ src/homepage.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const HomePage = () =>  {
+  return (
+    <div className='homepage'>
+      <div className='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1>HATS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1>JACKETS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1>SNEAKERS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1>WOMENS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1>MENS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage;
