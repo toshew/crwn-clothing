4b78596f0005ea4bbe7f5eb85a3f92d633f128ab
vladantosev <vladantosev@yahoo.com>
2019-12-30 23:52:30 +0100

    Added font and styling title in menu-item component

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -9,7 +9,7 @@ export const MenuItem = ({ title, imageUrl, size }) => {
     className={`menu-item ${size}`}
     >
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
