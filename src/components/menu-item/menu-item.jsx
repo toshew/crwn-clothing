03033d287a1d92a7a80779f344cb4a80e28551a8
vladantosev <vladantosev@yahoo.com>
2019-12-31 00:06:05 +0100

    Added hover effect with transition for menu-items

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -3,11 +3,13 @@ import './menu-item.scss';
 
 export const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div style={{
-      backgroundImage: `url(${imageUrl})`
-    }} 
-    className={`menu-item ${size}`}
-    >
+    <div className={`menu-item ${size}`}>
+      <div 
+        className='background-image' 
+        style={{
+        backgroundImage: `url(${imageUrl})`
+        }} 
+      />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
